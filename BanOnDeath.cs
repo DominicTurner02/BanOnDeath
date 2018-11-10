8ee77337c662a091972ee5ca86d6e5c71da1448d
MrKwabs <40438275+MrKwabs@users.noreply.github.com>
2018-11-10 15:20:53 +0000

    Added Main & Config.

---
+++ BanOnDeath.cs
@@ -0,0 +1,87 @@
+﻿using Rocket.Core.Plugins;
+using Rocket.Unturned.Player;
+using Logger = Rocket.Core.Logging.Logger;
+using Rocket.Unturned.Events;
+using Steamworks;
+using Rocket.API;
+using SDG.Unturned;
+
+
+namespace BanOnDeath
+{
+    public class BanOnDeath : RocketPlugin<ConfigurationBanOnDeath>
+    {
+        public static BanOnDeath Instance { get; private set; }
+
+        protected override void Load()
+        {
+            base.Load();
+            Instance = this;
+
+            Logger.LogWarning(" Loading BanOnDeath by Mr.Kwabs.");
+
+            Logger.LogWarning($"\n Ban Length: {Configuration.Instance.BanLengthSeconds} Seconds");
+            Logger.LogWarning($"\n Ban Reason: '{Configuration.Instance.BanReason}' \n");
+
+            if (Configuration.Instance.OnlyBanOnSuicide)
+            {
+                Logger.LogWarning(" Only Ban on Suicide: Enabled");
+            }
+            else
+            {
+                Logger.LogError(" Only Ban on Suicide: Disabled");
+            }
+            Logger.LogWarning("\n BanOnDeath by Mr.Kwabs has successfully loaded.");
+
+            UnturnedPlayerEvents.OnPlayerDeath += OnPlayerDeath;
+        }
+
+        protected override void Unload()
+        {
+            UnturnedPlayerEvents.OnPlayerDeath -= OnPlayerDeath;
+            base.Unload();
+            Instance = null;
+        }
+
+
+        private void OnPlayerDeath(UnturnedPlayer Player, EDeathCause Cause, ELimb Limb, CSteamID Murderer)
+        {
+
+            if (Configuration.Instance.OnlyBanOnSuicide)
+            {
+                if (Cause.ToString() == "SUICIDE" || UnturnedPlayer.FromCSteamID(Murderer) == Player)
+                {
+                    DeathBan(Player);
+                } else
+                {
+                    Logger.LogWarning("Not banning Player, as it wasn't suicide.");
+                }
+            }
+            else
+            {
+                DeathBan(Player);
+            }
+
+        }
+
+        private void DeathBan(UnturnedPlayer player)
+        {
+            if (!player.HasPermission("deathban.ignore"))
+            {
+                SteamBlacklist.ban(player.CSteamID, CSteamID.Nil, Configuration.Instance.BanReason, Configuration.Instance.BanLengthSeconds);
+                player.Ban(Configuration.Instance.BanReason, Configuration.Instance.BanLengthSeconds);
+                Logger.LogWarning($"{player.DisplayName} [{player.Id}] has been banned for {Configuration.Instance.BanLengthSeconds} seconds for dying!");
+            } else
+            {
+                Logger.LogWarning("Ignoring Death...");
+            }
+
+
+        
+        }
+
+    }
+
+  
+
+}
---
+++ ConfigurationBanOnDeath.cs
@@ -0,0 +1,19 @@
+﻿using Rocket.API;
+
+namespace BanOnDeath
+{
+    public class ConfigurationBanOnDeath : IRocketPluginConfiguration
+    {
+        public uint BanLengthSeconds;
+        public bool OnlyBanOnSuicide;
+        public string BanReason;
+
+        public void LoadDefaults()
+        {
+            BanLengthSeconds = 86400;
+            OnlyBanOnSuicide = false;
+            BanReason = "You died!";
+        }
+
+    }
+}
